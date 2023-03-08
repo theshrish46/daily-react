3d3d656db3f06e3bc31893045d4956e0dfcd0318
theshrish46 <theshrish46@gmail.com>
2023-03-08 19:37:01 +0530

    on 8-3-3

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react';
+import "./../style/Home.css";
 
 const Home = () => {
+  const [name, setName] = useState("Initial");
+  const [email, setEmail] = useState("xyz@mail.com");
+
   return (
     <div>
-      
+      <form>
+        <input type="text" placeholder='Initial' onChange={(e) => setName(e.target.value)}/>
+        <input type="Email" placeholder='xyz@gmail.com'onChange={(e) => setEmail(e.target.value)}/>
+      </form>
+      <button onClick={(e) => setName(e.target.value)}>Pressed</button>
+      <p>{name}</p>
+      <p>{email}</p>
     </div>
   )
 }
